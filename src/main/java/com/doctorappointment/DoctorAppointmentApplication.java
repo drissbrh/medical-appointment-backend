101d62bdf827c27e511c80320ac512f87a589cf7
drissbrh <barhoinedriss@gmail.com>
2023-10-17 01:21:50 +0100

    Added the corsConfig

--- src/main/java/com/doctorappointment/DoctorAppointmentApplication.java
+++ src/main/java/com/doctorappointment/DoctorAppointmentApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DoctorAppointmentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DoctorAppointmentApplication.class, args);
     }
-
 }
--- src/main/java/com/doctorappointment/enums/Speciality.java
+++ src/main/java/com/doctorappointment/enums/Speciality.java
@@ -1,15 +1,26 @@
 package com.doctorappointment.enums;
 
 public enum Speciality {
-    Allergy_And_Immunology("Allergy And Immunology"),Anesthesiology("Anesthesiology"),Colon_And_Rectal_Surgery("Colon And Rectal Surgery"),
-    Cardiology("Cardiology"),Dermatology("Dermatology"),Physical_Medicine_And_Rehabilitation("Physical Medicine And Rehabilitation"),Emergency_Medicine("Emergency Medicine"),
-    Internal_Medicine("Internal Medicine"),Neurology("Neurology"),Ophthalmology("Ophthalmology"),Psychiatry("Psychiatry"),Surgery("Surgery");
+    Allergy_And_Immunology("Allergy And Immunology"),
+    Anesthesiology("Anesthesiology"),
+    Colon_And_Rectal_Surgery("Colon And Rectal Surgery"),
+    Cardiology("Cardiology"),
+    Dermatology("Dermatology"),
+    Physical_Medicine_And_Rehabilitation("Physical Medicine And Rehabilitation"),
+    Emergency_Medicine("Emergency Medicine"),
+    Internal_Medicine("Internal Medicine"),
+    Neurology("Neurology"),
+    Ophthalmology("Ophthalmology"),
+    Psychiatry("Psychiatry"),
+    Surgery("Surgery");
 
     private final String speciality;
+
     Speciality(String speciality) {
-        this.speciality=speciality;
+        this.speciality = speciality;
     }
-    public String getSpeciality(){
+
+    public String getSpeciality() {
         return this.speciality;
     }
 }
